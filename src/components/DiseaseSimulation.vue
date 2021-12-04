bb4f97b74a4a6173e2478485a6a5d96d4480dddb
Barry Becker <barrybecker4@gmail.com>
2021-12-04 04:49:44 -0800

    Initial commit

---
+++ src/components/DiseaseSimulation.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-card class="card">
+      <div class="inputs">
+      <span class="input-line">The incidence of the disease in the population is
+         <span id="probability-diseased">{{initialPctDiseased}}%</span>
+         <span id="probability-diseased-slider" class="slider"></span>
+      </span>
+         <div class="input-line">The disease testing accuracy is <span id="test-accuracy">{{initialTestAccuracy}}%</span>
+             <div id="test-accuracy-slider" class="slider"></div>
+         </div>
+      </div>
+      <!--bayes-rule-view :graph="this.graph" :totalPopulation="this.totalPopulation"></bayes-rule-view>
+      <sankey-view :graph="this.graph" @highlight="onHighlight" @unhighlight="onUnhighlight"></sankey-view>
+      <venn-diagram-view :graph="this.graph" :totalPopulation="this.totalPopulation"></venn-diagram-view -->
+      <!--notes-content :testPositive="this.testPositive" :totalPopulation="this.totalPopulation"></notes-content-->
+  </q-card>
+</template>
+
+<script>
+
+import IntroductionContent from 'components/NotesContent.vue';
+
+export default {
+
+  // name: 'ComponentName',
+
+  components: {
+      //BayesRuleView,
+      //SankeyView,
+      //VennDiagramView,
+      //NotesContent
+  },
+
+  setup () {
+    return {}
+  }
+
+
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page>
+    <introduction-content/>
+    <disease-simulation
+            :total-population="10000000"
+            :initial-pct-diseased="1"
+            :initial-test-accuracy="90">
+    </disease-simulation>
+
+
+    <q-card class="card">
+      <q-card-section class="bg-blue">
+        Title
+      </q-card-section>
+      <q-card-section class="bg-orange">
+        Body
+      </q-card-section>
+      <q-card-section>
+        Body 2
+      </q-card-section>
+    </q-card>
+  </q-page>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import IntroductionContent from 'components/IntroductionContent.vue';
+import DiseaseSimulation from 'components/DiseaseSimulation.vue';
+
+
+export default defineComponent({
+
+  name: 'PageIndex',
+
+  components: {
+    IntroductionContent,
+    DiseaseSimulation,
+  },
+})
+</script>
+
+
+
+<style scoped>
+
+  /* class="col-xs-12 col-sm-6 col-md-4" */
+  .card-sm {
+    font-size: 12px;
+    margin: 10px;
+    padding: 10px;
+  }
+  .card {
+    font-size: 18px;
+    margin: 20px;
+    padding: 20px;
+    box-sizing: border-box;
+  }
+
+</style>
+
