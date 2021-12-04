3ff0ea9da50225e14fe8d5e7ce44ecc7221bb1f2
Barry Becker <barrybecker4@gmail.com>
2021-12-04 07:22:41 -0800

    add styles and images

--- src/components/DiseaseSimulation.vue
+++ src/components/DiseaseSimulation.vue
@@ -89,92 +89,91 @@ export default {
   },
 
   methods: {
-       init: function() {
-            this.initializeInputSection(this.initialPctDiseased, this.initialTestAccuracy);
-            this.updateViews();
-       },
+    init: function() {
+      this.initializeInputSection(this.initialPctDiseased, this.initialTestAccuracy);
+      this.updateViews();
+    },
 
-        /**
-         * Show two sliders that allow changing the incidence and accuracy.
-         */
-        initializeInputSection: function(initialPctDiseased, initialTestAccuracy) {
-            let probDiseasedSlider = $("#probability-diseased-slider");
-            let testAccuracySlider = $("#test-accuracy-slider");
+    /**
+     * Show two sliders that allow changing the incidence and accuracy.
+     */
+    initializeInputSection: function(initialPctDiseased, initialTestAccuracy) {
+      let probDiseasedSlider = $("#probability-diseased-slider");
+      let testAccuracySlider = $("#test-accuracy-slider");
 
-            // Using integer values to avoid rounding problems at the max value
-            probDiseasedSlider.slider({
-                value: Math.log10(initialPctDiseased),
-                min: -2,
-                max: 1.0,
-                step: 0.1,
-                height: "10px",
-                slide: this.getSliderChangedHandler("#probability-diseased", this.pctDiseasedConverter),
-                stop: this.clearThumbTip
-            });
+      // Using integer values to avoid rounding problems at the max value
+      probDiseasedSlider.slider({
+          value: Math.log10(initialPctDiseased),
+          min: -2,
+          max: 1.0,
+          step: 0.1,
+          height: "10px",
+          slide: this.getSliderChangedHandler("#probability-diseased", this.pctDiseasedConverter),
+          stop: this.clearThumbTip
+      });
 
-            testAccuracySlider.slider({
-                value: initialTestAccuracy * 10,
-                min: 800,
-                max: 999,
-                step: 1,
-                slide: this.getSliderChangedHandler("#test-accuracy", this.testAccuracyConverter),
-                stop: this.clearThumbTip
-            });
-        },
+      testAccuracySlider.slider({
+          value: initialTestAccuracy * 10,
+          min: 800,
+          max: 999,
+          step: 1,
+          slide: this.getSliderChangedHandler("#test-accuracy", this.testAccuracyConverter),
+          stop: this.clearThumbTip
+      });
+    },
 
-        pctDiseasedConverter: function(sliderValue) {
-            return diseaseConsts.format(Math.pow(10, sliderValue), 2) + "%";
-        },
+    pctDiseasedConverter: function(sliderValue) {
+      return diseaseConsts.format(Math.pow(10, sliderValue), 2) + "%";
+    },
 
-        testAccuracyConverter: function(sliderValue) {
-            return sliderValue / 10 + "%";
-        },
+    testAccuracyConverter: function(sliderValue) {
+      return sliderValue / 10 + "%";
+    },
 
-        /**
-         * @param sliderEl jquery selector for slider
-         * @param convert function used to map slider value to actual value
-         * @returns {Function} slider changed callback
-         */
-        getSliderChangedHandler: function(sliderEl, convert) {
-            let vm = this;
-            return function (event, ui) {
-                // update value in text
-                var value = convert(ui.value);
-                $(sliderEl).text(value);
+    /**
+     * @param sliderEl jquery selector for slider
+     * @param convert function used to map slider value to actual value
+     * @returns {Function} slider changed callback
+     */
+    getSliderChangedHandler: function(sliderEl, convert) {
+      let vm = this;
+      return function (event, ui) {
+        // update value in text
+        var value = convert(ui.value);
+        $(sliderEl).text(value);
 
-                // current value (when sliding) or initial value (at start)
-                var tooltip = '<div class="tooltip"><div class="tooltip-inner">' + value
-                    + '</div><div class="tooltip-arrow"></div></div>';
-                $(sliderEl + "-slider").find('.ui-slider-handle').html(tooltip);
-                vm.updateViews();
-            }
-        },
+        // current value (when sliding) or initial value (at start)
+        var tooltip = '<div class="tooltip"><div class="tooltip-inner">' + value
+            + '</div><div class="tooltip-arrow"></div></div>';
+        $(sliderEl + "-slider").find('.ui-slider-handle').html(tooltip);
+        vm.updateViews();
+      }
+    },
 
-        updateViews: function() {
-            this.probDiseased = parseFloat($("#probability-diseased").text()) / 100.0;
-            this.testAccuracy = parseFloat($("#test-accuracy").text()) / 100.0;
+    updateViews: function() {
+      this.probDiseased = parseFloat($("#probability-diseased").text()) / 100.0;
+      this.testAccuracy = parseFloat($("#test-accuracy").text()) / 100.0;
 
-            let links = [
-                {"source": 0, "target": 2, "value": this.testNegButDiseased},
-                {"source": 0, "target": 3, "value": this.testPositiveAndDiseased},
-                {"source": 1, "target": 3, "value": this.testPositiveButHealthy},
-                {"source": 1, "target": 4, "value": this.testNegAndHealthy}
-            ];
-            //Vue.set(this.graph, 'links', links); // no longer needed for Vue to recognizes changes in Vue 3
-        },
+      let links = [
+          {"source": 0, "target": 2, "value": this.testNegButDiseased},
+          {"source": 0, "target": 3, "value": this.testPositiveAndDiseased},
+          {"source": 1, "target": 3, "value": this.testPositiveButHealthy},
+          {"source": 1, "target": 4, "value": this.testNegAndHealthy}
+      ];
+      //Vue.set(this.graph, 'links', links); // no longer needed for Vue to recognizes changes in Vue 3
+    },
 
-        clearThumbTip: function(event, ui) {
-            $("#probability-diseased-slider").find('.ui-slider-handle').empty();
-            $("#test-accuracy-slider").find('.ui-slider-handle').empty();
-        },
+    clearThumbTip: function(event, ui) {
+      $("#probability-diseased-slider").find('.ui-slider-handle').empty();
+      $("#test-accuracy-slider").find('.ui-slider-handle').empty();
+    },
 
-        onHighlight(value) {
-          console.log("highlight:" + value);
-        },
-        onUnhighlight(value) {
-          console.log("unhighlight:" + value);
-        }
+    onHighlight(value) {
+      console.log("highlight:" + value);
+    },
+    onUnhighlight(value) {
+      console.log("unhighlight:" + value);
+    }
   }
-
 }
 </script>
